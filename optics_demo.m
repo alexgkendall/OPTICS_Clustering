2552a55cb0589a7478fe3d40db48e587feeca0a8
Alex Kendall <agk34@million.eng.cam.ac.uk>
2015-02-18 17:20:29 +0000

    Initial Commit

---
+++ optics_demo.m
@@ -0,0 +1,20 @@
+% Brief Demo to Visualise Optics Results
+
+% Written by Alex Kendall
+% University of Cambridge
+% 18 Feb 2015
+% http://mi.eng.cam.ac.uk/~agk34/
+
+% This software is licensed under GPLv3, see included glpv3.txt.
+
+% ::IMPORTANT:: load your data to 'points'
+[ SetOfClusters, RD, CD, order ] = cluster_optics(points, minpts, epsilon);
+
+bar(RD(order));
+figure;
+
+% Cycle through all clusters
+for i=2:length(SetOfClusters)
+    bar(RD(order(SetOfClusters(i).start:SetOfClusters(i).end)));
+    pause(0.5)
+end
